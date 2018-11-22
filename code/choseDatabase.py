e32d05a0a6cfcd8e99e7dc4b5326657afda5c370
Anastasiya Bordak <anastasiyabordak@gmail.com>
2018-11-23 02:36:47 +0300

    x, y data select + validate

--- code/choseDatabase.py
+++ code/choseDatabase.py
@@ -9,7 +9,10 @@ class choseDatabase(QtWidgets.QMainWindow):
 
     def __init__(self, mainWindow, server):
         super().__init__()
-        self.toolBox = QtWidgets.QToolBox(self.centralwidget)
+        self.xData = []
+        self.yData = []
+        self.allTabsName = []
+        self.alldatabases = []
         self.pages = []
         self.tabWidgets = []
         self.tableViews = []
@@ -28,11 +31,12 @@ class choseDatabase(QtWidgets.QMainWindow):
         cursor.execute(databases)
         for (databases) in cursor:
             pass_databases.append(databases[0])
+        self.alldatabases = pass_databases
         font = QtGui.QFont()
         font.setFamily("Bitstream Charter")
         font.setPointSize(18)
         font.setItalic(True)
-
+        self.toolBox = QtWidgets.QToolBox(self.centralwidget)
         self.toolBox.setGeometry(QtCore.QRect(0, 0, 650, 640))
         self.toolBox.setObjectName("toolBox")
         self.toolBox.setFont(font)
@@ -56,6 +60,7 @@ class choseDatabase(QtWidgets.QMainWindow):
             table_names = []
             for (table_name,) in cursor:
                 table_names.append(table_name)
+                self.allTabsName.append(table_name)
             for j in range(0, len(table_names)):
                 tab = QtWidgets.QWidget(self.tabWidgets[i])
                 tab.setObjectName("tab")
@@ -66,54 +71,62 @@ class choseDatabase(QtWidgets.QMainWindow):
                 cursor.execute("SHOW columns FROM " + table_names[j])
                 columns = []
                 for column in cursor:
-                    columns.append(column[0])
+                    columns.append(column[0] + ' TYPE: ' + column[1])
                 tableView.addItems(columns)
                 tableView.itemClicked.connect(self.itemClickedList)
                 self.tableViews.append(tableView)
-        self.comboBox.addItems(pass_databases)
         self.backButton.clicked.connect(self.showMainWindow)
-        self.submitButton.clicked.connect(self.choseData)
-        self.setFixedSize(953, 674)
+        self.submitButton.clicked.connect(self.sendData)
+        self.setFixedSize(1067, 654)
         self.show()
+
+    def sendData(self):
+        if len(self.xData) != len(self.yData):
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Critical)
+            msg.setText("Can't create plot, check len")
+            msg.setWindowTitle("ERROR")
+            msg.setStandardButtons(QMessageBox.Close)
+            retval = msg.exec_()
 
     def itemClickedList(self, item):
         cwidget = self.sender().parent()
-        print(cwidget)
-        print(item.text())
-        print(item)
-        for i in self.tableViews:
-            if i is cwidget:
-                print("T")
-        for i in self.tabs:
-            if i is cwidget:
-                print("Y")
-        print(cwidget.parent())
+        listCTD = []
+
+        listCTD.append(item.text().split(' ')[0])
+        if 'decimal' in item.text().split(' ')[2] or 'int' in item.text().split(' ')[2]:
+            for i in range(0, len(self.tabs)):
+                if self.tabs[i] is cwidget:
+                    listCTD.append(self.allTabsName[i])
+            listCTD.append(self.alldatabases[self.toolBox.currentIndex()])
+            msgBox = QMessageBox()
+            msgBox.setText('What to do?')
+
+            msgBox.addButton(QtWidgets.QPushButton('Abscissa'), QMessageBox.YesRole)  # 0
+            msgBox.addButton(QtWidgets.QPushButton('Ordinate'), QMessageBox.NoRole)  # 1
+            msgBox.addButton(QtWidgets.QPushButton('Cancel'), QMessageBox.RejectRole)  # 2
+            ret = msgBox.exec_()
+            if ret == 0:
+                self.xData.append(listCTD)
+                self.xEdit.insertPlainText(listCTD[2] + "." + listCTD[1] + "." + listCTD[0])
+                self.xEdit.insertPlainText("\n")
+            if ret == 1:
+                self.yData.append(listCTD)
+                self.yEdit.insertPlainText(listCTD[2] + "." + listCTD[1] + "." + listCTD[0])
+                self.yEdit.insertPlainText("\n")
+        else:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Critical)
+            msg.setText("Can't create plot, check type")
+            msg.setWindowTitle("ERROR")
+            msg.setStandardButtons(QMessageBox.Close)
+            retval = msg.exec_()
 
     # Back to main
     def showMainWindow(self):
         global main_ui
         self.mainWindow.show()
         self.close()
-
-    def choseData(self):
-
-        cursor = self.server.cursor()  # get the cursor
-        print("USE " + self.comboBox.currentText())
-        cursor.execute("USE "
-                       + self.comboBox.currentText())  # select the database
-        cursor.execute("SHOW TABLES")
-        # execute 'SHOW TABLES' (but data is not returned)
-        table_names = []
-        for (table_name,) in cursor:
-            print("TABLE NAME", table_name)
-            table_names.append(table_name)
-        for table_name in table_names:
-            print("TABLE NAME", table_name)
-            cursor.execute("SHOW columns FROM " + table_name)
-            print("Column names:")
-            for column in cursor:
-                print(column)
-        pass
 
 
 if __name__ == "__main__":
